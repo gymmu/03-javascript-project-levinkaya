be2e6acbd0ff75da5df3eb99c46183b152fe2648
levin kaya <e227478@edu.sbl.ch>
2023-10-30 12:01:10 +0100

    Aufgabe 1&2

--- script.js
+++ script.js
@@ -2,9 +2,38 @@ export function aufgabe01(args) {
   const input = args
   const result = []
 
-  for (let i = 0; i < input.length; i++) {
+ for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
-    result.push(currentElement)
+    
+
+  //Wenn das aktuelle Zeichen ein 'e' ist, hänge nichts an die Liste an
+    if (currentElement ==='e') {
+      result .push("")
+  //Wenn das aktuelle Zeichen ein 'E' ist, hänge nichts an die Liste an
+    } else if (currentElement === 'E') {
+     result.push("")
+    } else { // Das aktuelle Zeichen ist weder ein 'e' noch ein 'E' , hänge also das aktuelle zeichen
+    result. push(currentElement)
+    }
   }
+     
   return result.join("")
 }
+
+export function aufgabe02(args) {
+  const input = args
+  const result = []
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+  //Wandle das aktuelle Element in einen Grossbuchstaben um
+  const capitalizedElement = currentElement.toUpperCase()
+
+  //Hänge den Grossbuchstaben an das Resultat an...
+  result.push(capitalizedElement)
+  }
+
+ return result.join("")
+}
+
+

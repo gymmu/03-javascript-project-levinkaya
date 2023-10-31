6c7b8b2ffb1882c7f5b10d802df60c2b09b8e5a5
levin kaya <e227478@edu.sbl.ch>
2023-10-31 09:30:14 +0100

    Ende Lektion

--- script.js
+++ script.js
@@ -57,11 +57,34 @@ return count
  export function aufgabe04(args) {
   const input = args
   const result = []
- }
+  }
 
  export function aufgabe05(args) {
   const input = args
   const result = []
-   }
+  let capitalLetters = 0
 
-   
+//Setzte den counter auf 0
+for (let i = 0; i < input.length; i++) {
+  const currentElement = input[i]
+  if ( currentElement ==="."){
+    //do nothing
+  }else if(currentElement=== " "){
+    // ignoriere Leerschläge
+  }else if(currentElement=== currentElement.toUpperCase()) {
+    capitalLetters++
+
+
+}
+}
+
+
+if (capitalLetters > 0){
+  return true
+} else {
+  return false
+}
+
+ }
+
+

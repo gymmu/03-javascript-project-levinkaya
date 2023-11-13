3b3e8fbca8725d05650b56903c7199f6a9eb5738
levin kaya <e227478@edu.sbl.ch>
2023-11-13 11:37:11 +0100

    Aufgabe 9,11,12

--- script.js
+++ script.js
@@ -75,15 +75,14 @@ for (let i = 0; i < input.length; i++) {
     capitalLetters++
  }
 }
-
-}
-
-
 if (capitalLetters > 0){
   return true
 } else {
   return false
 }
+}
+
+
 
  
 
@@ -102,8 +101,9 @@ if (capitalLetters > 0){
   return result.join("")
  }
 
-
  }
+
+
 
 export function aufgabe08(args) {
   const input = args
@@ -126,4 +126,54 @@ export function aufgabe08(args) {
  
 }
 return result.join("")
+
 }
+
+//Überprüfe, ob eine Angabe genau sechs Zeichen beträgt.
+export function aufgabe09(args) {
+  const input = args
+  let len = 0
+  for (let i = 0; i < input.length; i++) { 
+ len++
+  }
+
+  if (len=== 6) { 
+   return true
+ } else { 
+    return false
+    }
+    }
+
+    export function aufgabe11(args) {
+      //Gib den ASCII code eines einzelnen Buchstabens an.
+      const input = args
+      let ascii = null
+
+      if ( input.length > 1) { //Funktionswächter
+        return null
+      }
+       
+
+      for (let i = 0; i < input.length; i++) { 
+     const currentElement = input [i]
+     ascii = currentElement.charCodeAt(0)
+     return ascii
+      }
+  return null
+   }
+    
+
+
+   export function aufgabe12 (args) {
+    const input = args
+
+    
+    for (let i = 0; i < input.length; i++) {
+      const currentElement = input[i]
+      if (currentElement === 'e') { 
+        return i
+      }
+
+    }
+      return -1
+   }
